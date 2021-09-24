6741c7b41ee65a67936eefcd07292f59db40a80e
Avery Madison Decker <hello@avery.computer>
2021-09-24 11:37:25 -0400

    File organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,141 +1,17 @@
-import React, { Suspense, useRef, useState } from 'react';
-import { MathUtils, Mesh, Vector2, Vector3 } from 'three';
-import { Bloom, ChromaticAberration, EffectComposer, Noise } from '@react-three/postprocessing';
-import { Canvas, MeshProps, useFrame, useThree } from '@react-three/fiber';
-import { MeshDistortMaterial } from '@react-three/drei';
+import { Scene } from './Scene';
 import { ReactComponent as Wordmark } from './wordmark.svg';
 import './App.css';
-
-const Scene = () => {
-  const Tetrahedron = (props: MeshProps) => {
-    const ref = useRef<Mesh>();
-    useFrame(() => {
-      if (ref.current) ref.current.rotation.x = ref.current.rotation.y += 0.001;
-    });
-
-    return (
-      <mesh ref={ref} {...props}>
-        <tetrahedronGeometry />
-        <meshBasicMaterial transparent wireframe />
-      </mesh>
-    );
-  };
-
-  const Icosahedron = (props: MeshProps) => {
-    const ref = useRef<Mesh>();
-    useFrame(() => {
-      if (ref.current) ref.current.rotation.x = ref.current.rotation.y += 0.001;
-    });
-
-    return (
-      <mesh ref={ref} {...props}>
-        <icosahedronGeometry />
-        <meshBasicMaterial transparent wireframe />
-      </mesh>
-    );
-  };
-
-  const Octahedron = (props: MeshProps) => {
-    const ref = useRef<Mesh>();
-    useFrame(() => {
-      if (ref.current) ref.current.rotation.x = ref.current.rotation.y += 0.001;
-    });
-
-    return (
-      <mesh ref={ref} {...props}>
-        <octahedronGeometry />
-        <meshBasicMaterial transparent wireframe />
-      </mesh>
-    );
-  };
-
-  const Core = () => {
-    const ref = useRef<Mesh>();
-    useFrame(() => {
-      if (ref.current) ref.current.rotation.x = ref.current.rotation.y += 0.001;
-    });
-
-    return (
-      <mesh ref={ref}>
-        <sphereGeometry args={[3, 50, 50]} />
-        {
-          // This type has an error that needs fixing https://github.com/pmndrs/drei/issues/553
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          <MeshDistortMaterial color="#dddddd" emissive="#dddddd" eattach="material" transparent opacity={0.9} speed={5} distort={0.2} />
-        }
-      </mesh>
-    );
-  };
-
-  const Rig: React.FC = ({ children }) => {
-    const ref = useRef<Mesh>();
-    const vec = new Vector3();
-    const { camera, mouse } = useThree();
-
-    const DEFAULTZOOM = 3.5;
-    const zoom = window.innerWidth > 800 ? DEFAULTZOOM : DEFAULTZOOM + 2;
-
-    useFrame(() => {
-      camera.position.lerp(vec.set(mouse.x * 2, 0, zoom), 0.05);
-      if (ref.current) {
-        ref.current.position.lerp(vec.set(mouse.x * 1, mouse.y * -1, -zoom), 0.1);
-        ref.current.rotation.y = MathUtils.lerp(ref.current.rotation.y, (mouse.x * Math.PI) / 20, 0.1);
-      }
-    });
-
-    return <group ref={ref}>{children}</group>;
-  };
-
-  const Effects = () => {
-    const { camera } = useThree();
-    const abberationOffsetIntensity = 0.01;
-    const [abberationOffset, setAbberationOffset] = useState([0, 0]);
-
-    useFrame(() => setAbberationOffset([camera.position.x * abberationOffsetIntensity, camera.position.y * abberationOffsetIntensity]));
-
-    return (
-      <EffectComposer>
-        <Bloom luminanceSmoothing={0.9} height={400} />
-        <Noise opacity={0.1} />
-        <ChromaticAberration offset={new Vector2(...abberationOffset)} />
-      </EffectComposer>
-    );
-  };
-
-  return (
-    <Canvas dpr={Math.max(window.devicePixelRatio, 2)}>
-      <color attach="background" args={["#010101"]} />
-      <Suspense fallback={null}>
-        <Rig>
-          <Tetrahedron position={[8, -4, -2]} />
-          <Tetrahedron position={[4, -7, -6]} />
-          <Tetrahedron position={[5, 6, -4]} />
-          <Tetrahedron position={[-8, 9, -5]} />
-          <Tetrahedron position={[-2, 6, -7]} />
-          <Octahedron position={[4, -6, 2]} />
-          <Octahedron position={[-10, -6, -6]} />
-          <Icosahedron position={[1, 7, 0]} />
-          <Icosahedron position={[4, 0.5, 3]} />
-          <Icosahedron position={[-1, -4, 2.5]} />
-          <Icosahedron position={[-2, 1, 5]} />
-          <Core />
-        </Rig>
-        <Effects />
-      </Suspense>
-    </Canvas >
-  );
-};
 
 const App = () => {
   return (
     <div className="app">
-      <div className="app-scene">
+      <div className="scene">
         <Scene />
       </div>
-      <header className="app-header">
+      <header className="header">
         <Wordmark />
       </header>
+      <button className="button">About</button>
     </div>
   );
 };
---
+++ src/Scene.tsx
@@ -0,0 +1,66 @@
+import React, { Suspense, useRef, useState } from 'react';
+import { MathUtils, Mesh, Vector2, Vector3 } from 'three';
+import { Bloom, ChromaticAberration, EffectComposer, Noise } from '@react-three/postprocessing';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { BlobShape, WireframeShape } from './shapes';
+import './App.css';
+
+export const Scene: React.FC = () => {
+  const Rig: React.FC = ({ children }) => {
+    const ref = useRef<Mesh>();
+    const vec = new Vector3();
+    const { camera, mouse } = useThree();
+
+    const DEFAULTZOOM = 3.5;
+    const zoom = window.innerWidth > 800 ? DEFAULTZOOM : DEFAULTZOOM + 2;
+
+    useFrame(() => {
+      camera.position.lerp(vec.set(mouse.x * 2, 0, zoom), 0.05);
+      if (ref.current) {
+        ref.current.position.lerp(vec.set(mouse.x * 1, mouse.y * -1, -zoom), 0.1);
+        ref.current.rotation.y = MathUtils.lerp(ref.current.rotation.y, (mouse.x * Math.PI) / 20, 0.1);
+      }
+    });
+
+    return <group ref={ref}>{children}</group>;
+  };
+
+  const Effects = () => {
+    const { camera } = useThree();
+    const abberationOffsetIntensity = 0.01;
+    const [abberationOffset, setAbberationOffset] = useState([0, 0]);
+
+    useFrame(() => setAbberationOffset([camera.position.x * abberationOffsetIntensity, camera.position.y * abberationOffsetIntensity]));
+
+    return (
+      <EffectComposer>
+        <Bloom luminanceSmoothing={0.9} height={400} />
+        <Noise opacity={0.1} />
+        <ChromaticAberration offset={new Vector2(...abberationOffset)} />
+      </EffectComposer>
+    );
+  };
+
+  return (
+    <Canvas dpr={Math.max(window.devicePixelRatio, 2)}>
+      <color attach="background" args={["#010101"]} />
+      <Suspense fallback={null}>
+        <Rig>
+          <WireframeShape type="tetrahedron" position={[8, -4, -2]} />
+          <WireframeShape type="tetrahedron" position={[4, -7, -6]} />
+          <WireframeShape type="tetrahedron" position={[5, 6, -4]} />
+          <WireframeShape type="tetrahedron" position={[-8, 9, -5]} />
+          <WireframeShape type="tetrahedron" position={[-2, 6, -7]} />
+          <WireframeShape type="octahedron" position={[4, -6, 2]} />
+          <WireframeShape type="octahedron" position={[-10, -6, -6]} />
+          <WireframeShape type="icossahedron" position={[1, 7, 0]} />
+          <WireframeShape type="icossahedron" position={[4, 0.5, 3]} />
+          <WireframeShape type="icossahedron" position={[-1, -4, 2.5]} />
+          <WireframeShape type="icossahedron" position={[-2, 1, 5]} />
+          <BlobShape />
+        </Rig>
+        <Effects />
+      </Suspense>
+    </Canvas >
+  );
+};
